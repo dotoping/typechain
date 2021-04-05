12e0fe9056cda275edefb0e2ecd1f194262a58a5
Daniel <coding.dotping@gmail.com>
2021-04-05 16:52:11 +1000

    use interface and class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'Daniel',
-  age: 27,
-  gender: 'male',
-};
+const tori = new Human('Tori', 3, 'male');
 
 const sayHello = (person: Human): string => {
   return `My name is ${person.name}. I am ${person.age} and ${person.gender}.`;
 };
 
-console.log(sayHello(person));
+console.log(sayHello(tori));
